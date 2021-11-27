9b27b6f38b716073f031fa8f336423b3c41838b4
azzumwaqar <azzumwaqar@gmail.com>
2021-11-27 19:42:02 +0000

    added espresso BaseTestClass.kt EspressoDsl.kt @LargeTests

---
+++ DiceRoller/app/src/androidTest/java/com/example/diceroller/MainActivityTest.kt
@@ -0,0 +1,32 @@
+package com.example.diceroller
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+
+class MainActivityTest : BaseTestClass(){
+
+
+    @Rule @JvmField
+    val rule = ActivityTestRule(MainActivity::class.java)
+//    @get:Rule
+//    val scenarioRule = ActivityScenarioRule(MainActivity::class.java).scenario.moveToState(Lifecycle.State.RESUMED)
+
+    @Test
+    fun checkTextViewIsEmpty() {
+
+        onView(withId(R.id.textView))
+            .check(matches(withText("")))
+    }
+
+    @Test
+    fun clickRollButton() {
+        onView(withId(R.id.button)).perform(click())
+    }
+}
