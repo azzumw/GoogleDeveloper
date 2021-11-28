329417eea096a8e5e580d38a0ec6b7fba55a0fdd
azzumwaqar <azzumwaqar@gmail.com>
2021-11-28 18:11:07 +0000

    debugImplementation "androidx.test:core:1.3.0" added to gradle activityScenario working

--- DiceRoller/app/src/androidTest/java/com/example/diceroller/MainActivityTest.kt
+++ DiceRoller/app/src/androidTest/java/com/example/diceroller/MainActivityTest.kt
@@ -1,11 +1,12 @@
 package com.example.diceroller
 
+import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.rule.ActivityTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,20 +16,19 @@ class MainActivityTest : BaseTestClass(){
 //    TODO (1): add testImageResourceID
 //    TODO(2): addtestCorrectImageShown
 
-    @Rule @JvmField
-    val rule = ActivityTestRule(MainActivity::class.java)
-//    @get:Rule
-//    val scenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    @get:Rule
+    val scenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun checkTextViewIsEmpty() {
-
+//        launch(MainActivity::class.java)
         onView(withId(R.id.diceImageView))
-            .check(matches(withText("")))
+            .check(matches(withContentDescription(R.string.dice_image_text)))
     }
 
     @Test
     fun clickRollButton() {
+        launch(MainActivity::class.java)
         onView(withId(R.id.button)).perform(click())
     }
 
