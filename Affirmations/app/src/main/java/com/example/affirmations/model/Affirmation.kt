e065e82ca64a782e8b20ad1ffc5386d9aa2bd0ce
azzumwaqar <azzumwaqar@gmail.com>
2021-12-12 18:43:58 +0000

    Datasource.kt implemented Affirmation.kt data class added bindingView added

---
+++ Affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.example.affirmations.model
+
+/**
+ * To ensure consistency and meaningful behavior of the generated code, data classes have to fulfill the following requirements:
+ * The primary constructor needs to have at least one parameter.
+ * All primary constructor parameters need to be marked as val or var.
+ * Data classes cannot be abstract, open, sealed, or inner.
+ * */
+data class Affirmation(val stringResourceId: Int)
