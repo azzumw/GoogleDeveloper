ea0d5bbfaabfd7e43d5749824626bfdf50dec105
azzumwaqar <azzumwaqar@gmail.com>
2021-12-14 21:53:43 +0000

    14/12 - added dice property to Dice application

--- DiceRoller/app/src/main/java/com/example/diceroller/DiceApplication.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/DiceApplication.kt
@@ -4,8 +4,13 @@ import android.app.Application
 import android.util.Log
 
 class DiceApplication : Application() {
+
+    lateinit var dice:Dice
+
     override fun onCreate() {
         super.onCreate()
         Log.d(this.javaClass.canonicalName,"Hello")
+
+        dice = RealDice()
     }
 }
