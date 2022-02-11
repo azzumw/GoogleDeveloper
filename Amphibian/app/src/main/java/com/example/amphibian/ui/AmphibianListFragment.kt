a504c4ccc3494660a55e2380d48d8f7477f6bbe2
azzumwaqar <azzumwaqar@gmail.com>
2022-02-11 10:57:34 +0000

    Lemonade Tests Passing

--- Amphibian/app/src/main/java/com/example/amphibian/ui/AmphibianListFragment.kt
+++ Amphibian/app/src/main/java/com/example/amphibian/ui/AmphibianListFragment.kt
@@ -18,7 +18,6 @@ class AmphibianListFragment : Fragment() {
 
     private val viewModel: AmphibianViewModel by activityViewModels()
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
