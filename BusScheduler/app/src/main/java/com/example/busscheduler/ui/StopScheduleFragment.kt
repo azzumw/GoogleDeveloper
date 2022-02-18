04fda5e4310f13ea93f193f3be078f290d1d6b15
azzumwaqar <azzumwaqar@gmail.com>
2022-02-18 15:47:04 +0000

    setting up StopScheduleFragment.kt

--- BusScheduler/app/src/main/java/com/example/busscheduler/ui/StopScheduleFragment.kt
+++ BusScheduler/app/src/main/java/com/example/busscheduler/ui/StopScheduleFragment.kt
@@ -5,13 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.RecyclerView
+import com.example.busscheduler.adapter.BusStopAdapter
 import com.example.busscheduler.databinding.FragmentFullScheduleBinding
+import com.example.busscheduler.viewmodels.BusScheduleViewModel
+import com.example.busscheduler.viewmodels.BusScheduleViewModelFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class StopScheduleFragment : Fragment() {
 
     companion object {
         var STOP_NAME = "stopName"
+    }
+
+    private val viewModel : BusScheduleViewModel by activityViewModels {
+        BusScheduleViewModelFactory((activity?.application as BusScheduleApplication).database.scheduleDao())
     }
 
     private var _binding : FragmentFullScheduleBinding? = null
@@ -45,6 +56,16 @@ class StopScheduleFragment : Fragment() {
         recyclerView = binding.recyclerView
 
 //        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val busStopAdapter = BusStopAdapter{}
+        recyclerView.adapter = busStopAdapter
+
+        // submitList() is a call that accesses the database. To prevent the
+// call from potentially locking the UI, you should use a
+// coroutine scope to launch the function. Using GlobalScope is not
+// best practice, and in the next step we'll see how to improve this.
+        GlobalScope.launch(Dispatchers.IO) {
+            busStopAdapter.submitList(viewModel.scheduleForStopName(stopName))
+        }
     }
 
     override fun onDestroy() {
