4a2f234f9129105ea6b89076df26106dcf1767d0
azzumwaqar <azzumwaqar@gmail.com>
2022-02-19 18:29:14 +0000

    InventoryViewModel.kt updated with addNewItem

--- InventoryApp/app/src/main/java/com/example/inventoryapp/viewmodels/InventoryViewModel.kt
+++ InventoryApp/app/src/main/java/com/example/inventoryapp/viewmodels/InventoryViewModel.kt
@@ -1,8 +1,29 @@
 package com.example.inventoryapp.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.inventoryapp.data.Item
 import com.example.inventoryapp.data.ItemDao
+import kotlinx.coroutines.launch
 
 class InventoryViewModel(private val itemDao: ItemDao) : ViewModel(){
 
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String){
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+
+    private fun insertItem(item:Item){
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+
+    private fun getNewItemEntry(name:String,price:String,qty:String):Item{
+        return Item(
+            name = name,
+            price = price.toDouble(),
+            quantity = qty.toInt()
+        )
+    }
 }
