3133077f282c4ec1806b07e74c3eafe97cd2b996
azzumwaqar <azzumwaqar@gmail.com>
2022-02-19 13:50:39 +0000

    AddItemFragment.kt added strings.xml updated

--- InventoryApp/app/src/main/java/com/example/inventoryapp/AddItemFragment.kt
+++ InventoryApp/app/src/main/java/com/example/inventoryapp/AddItemFragment.kt
@@ -5,35 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.example.inventoryapp.databinding.FragmentItemListBinding
+import com.example.inventoryapp.databinding.FragmentAddItemBinding
 
+class AddItemFragment : Fragment() {
 
-class ItemListFragment : Fragment() {
-
-    private var _binding : FragmentItemListBinding? = null
+    private var _binding : FragmentAddItemBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentItemListBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
+        _binding = FragmentAddItemBinding.inflate(inflater,container,false)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        recyclerView = binding.recyclerView
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
+
 }
