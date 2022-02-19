a304257959e15484acbaa64139fc5ef18a29bfe7
azzumwaqar <azzumwaqar@gmail.com>
2022-02-19 18:09:04 +0000

    added ViewModels and ViewModelFactory

---
+++ InventoryApp/app/src/main/java/com/example/inventoryapp/viewmodels/InventoryViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.inventoryapp.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.inventoryapp.data.ItemDao
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel(){
+
+}
---
+++ InventoryApp/app/src/main/java/com/example/inventoryapp/viewmodels/InventoryViewModelFactory.kt
@@ -0,0 +1,19 @@
+package com.example.inventoryapp.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.inventoryapp.data.ItemDao
+import java.lang.IllegalArgumentException
+
+class InventoryViewModelFactory (private val itemDao: ItemDao)
+    :ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(InventoryViewModel::class.java)){
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+
+        throw IllegalArgumentException("Unknown View model class")
+    }
+
+}
