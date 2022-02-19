16e911decc3b873bb4b7fe41ade692b294bd7506
azzumwaqar <azzumwaqar@gmail.com>
2022-02-19 19:15:36 +0000

    added extension function getFormattedPrice to Item.kt

--- InventoryApp/app/src/main/java/com/example/inventoryapp/data/Item.kt
+++ InventoryApp/app/src/main/java/com/example/inventoryapp/data/Item.kt
@@ -3,6 +3,7 @@ package com.example.inventoryapp.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.NumberFormat
 
 @Entity(tableName = "item")
 data class Item(
@@ -12,3 +13,6 @@ data class Item(
     @ColumnInfo (name = "price") val price:Double,
     @ColumnInfo (name = "quantity") val quantity:Int
 )
+
+fun Item.getFormattedPrice(): String =
+    NumberFormat.getCurrencyInstance().format(price)
