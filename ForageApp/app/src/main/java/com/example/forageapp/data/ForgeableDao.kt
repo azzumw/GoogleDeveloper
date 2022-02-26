8cec6cb48349cffdfc591fbacd1d6cb67352c216
azzumwaqar <azzumwaqar@gmail.com>
2022-02-26 18:38:09 +0000

    Entity created

--- ForageApp/app/src/main/java/com/example/forageapp/data/ForgeableDao.kt
+++ ForageApp/app/src/main/java/com/example/forageapp/data/ForgeableDao.kt
@@ -1,4 +1,5 @@
 package com.example.forageapp.data
 
 interface ForgeableDao {
+
 }
--- ForageApp/app/src/main/java/com/example/forageapp/model/Forageable.kt
+++ ForageApp/app/src/main/java/com/example/forageapp/model/Forageable.kt
@@ -1,9 +1,16 @@
 package com.example.forageapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "forgeable_database")
 data class Forageable(
+    @PrimaryKey (autoGenerate = true)
     val id: Long = 0,
     val name: String,
     val address:String,
+    @ColumnInfo(name ="in_season")
     val inSeason: Boolean,
     val notes:String?
 )
